119c65a9d9f046642f52b55a4b65fc3e75190a7c
Matt Payne <Payne@MattPayne.org>
2022-08-14 20:26:59 -0500

    Now passing in the name of the metric.

--- src/app/metric/metric.component.ts
+++ src/app/metric/metric.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-metric',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./metric.component.scss']
 })
 export class MetricComponent implements OnInit {
+  @Input('name') name = 'ST';
 
   constructor() { }
 
