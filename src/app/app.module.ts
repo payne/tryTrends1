55caa359ae6de0650086b20705a11a906da4db37
Matt Payne <Payne@MattPayne.org>
2022-08-14 19:55:05 -0500

    ng generate component metric

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MetricComponent } from './metric/metric.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MetricComponent
   ],
   imports: [
     BrowserModule,
